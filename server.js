2def92dcd4a106b107db0f4a5002126d427bb901
eSponjosa <e.rojascisterna@outlook.com>
2024-05-11 18:19:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -76,9 +76,9 @@ app.get('/api/registro', (req, res) => {
 });
 
 app.post('/api/insertRegistro', (req, res) => {
-  const { rut, entrada, salida, tipo, nDocumento} = req.body; // Datos del cuerpo de la solicitud
+  const { empleado_id_emp, espacio_id_esp, fecha_salida, fecha_entrada} = req.body; // Datos del cuerpo de la solicitud
   const query = 'INSERT INTO REGISTRO (id_registro, empleado_id_emp, espacio_id_esp, fecha_salida, fecha_entrada) VALUES (default, $1,$2,$3,$4)';
-  pool.query(query, [rut, entrada, salida, tipo, nDocumento], (error, results) => {
+  pool.query(query, [empleado_id_emp, espacio_id_esp, fecha_salida, fecha_entrada], (error, results) => {
     if (error) {
       console.error('Error al insertar datos: ', error);
       res.status(500).json({ error: 'Error al insertar datos' });
